646dc6162b5dd3d418455d3c6af73eb21269584c
Oleg Yagolnikov <oleg.yagolnikov@mail.ru>
2023-06-15 07:21:01 +0300

    изменения в index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import './index.css'; //импорт стилей
+import './index.css'; // импорт стилей
 
 import {
   buttons,
@@ -23,18 +23,22 @@ import PopupWithForm from '../components/PopupWithForm';
 import PopupWithConfirm from '../components/PopupWithConfirm';
 import Section from '../components/Section';
 import UserInfo from '../components/UserInfo';
-import { handleSubmit } from '../utils/utils';
+import { handleSubmit as handleFormSubmit } from '../utils/utils'; // Переименовали функцию handleSubmit
 
 let userId;
 // экземпляр api
 const api = new Api(config);
 // экземпляр данных пользователя
-const user = new UserInfo(profileObj.heading, profileObj.description, profileObj.avatar);
+const user = new UserInfo({
+  heading: profileObj.heading,
+  description: profileObj.description,
+  avatar: profileObj.avatar
+});
 // экземпляр для рендера карточек
 const cardSection = new Section('.elements', renderCard);
 // экземпляры модальных окон
 const popupEditProfile = new PopupWithForm(popupSelectors.editProfile, submitPopupProfile);
-const popupAddCard = new PopupWithForm(popupSelectors.addCard, sibmitPopupAddCard);
+const popupAddCard = new PopupWithForm(popupSelectors.addCard, submitPopupAddCard);
 const popupEditAvatar = new PopupWithForm(popupSelectors.editAvatar, submitPopupAvatar);
 const popupImage = new PopupWithImage(popupSelectors.imgContainer);
 const popupConfirm = new PopupWithConfirm(popupSelectors.deleteConfirm, submitDeleteCard);
@@ -45,7 +49,7 @@ popupEditAvatar.setEventListeners();
 popupImage.setEventListeners();
 popupConfirm.setEventListeners();
 
-//экземпляры форм валидации
+// экземпляры форм валидации
 const formValidProfile = new FormValidator(formObj, popupEditProfile.popup);
 const formValidCard = new FormValidator(formObj, popupAddCard.popup);
 const formValidAvatar = new FormValidator(formObj, popupEditAvatar.popup);
@@ -54,61 +58,65 @@ formValidProfile.enableValidation();
 formValidCard.enableValidation();
 formValidAvatar.enableValidation();
 
-buttons.editBtn.addEventListener('click', () => { handleEditProfile() });
+buttons.editBtn.addEventListener('click', handleEditProfile); // Убрали круглые скобки
 buttons.addBtn.addEventListener('click', () => { popupAddCard.open()});
 buttons.avatarBtn.addEventListener('click', () => { popupEditAvatar.open() });
 
 Promise.all([api.getProfile(), api.getCards()])
-.then(([userData, cards]) => {
-  user.setUserInfo(userData);
-  user.renderUserInfo();
-  user.setUserAvatar();
-  cardSection.renderItems(cards);
-})
-.catch(err => console.log(err))
+  .then(([userData, cards]) => {
+    user.setUserInfo(userData);
+    user.renderUserInfo();
+    user.setUserAvatar();
+    cardSection.renderItems(cards);
+  })
+  .catch(err => console.log(err))
 
-function submitPopupProfile(evt, {name, about}) {
+function submitPopupProfile(evt, { name, about }) {
   function makePatchProfile() {
     return api.patchProfile(name, about)
-    .then(data => {
-      user.setUserInfo({name: data.name, about: data.about});
-      user.renderUserInfo();
-    })
+      .then(data => {
+        user.setUserInfo({ name: data.name, about: data.about });
+        user.renderUserInfo();
+      })
+      .catch(err => {
+        console.error(`Ошибка: ${err}`);
+        throw err; // Пробросить ошибку дальше
+      });
   }
-  handleSubmit(makePatchProfile, evt, popupEditProfile);
-};
+  
+  handleFormSubmit(makePatchProfile, evt, popupEditProfile); // Исправили вызов функции handleSubmit
+}
 
-function sibmitPopupAddCard(evt, {link, caption}) {
+function submitPopupAddCard(evt, { link, caption }) {
   function makePostCard() {
-      return api.postCard( link, caption)
-      .then(data => {renderCard({ data, position: 'prepend'})
-    })
+    return api.postCard(link, caption)
+      .then(data => { renderCard({ data, position: 'prepend' }) })
   };
-  handleSubmit(makePostCard, evt, popupAddCard);
-};
+  handleFormSubmit(makePostCard, evt, popupAddCard); // Исправили вызов функции handleSubmit
+}
 
-function submitPopupAvatar(evt, {avatar} ) {
+function submitPopupAvatar(evt, { avatar }) {
   function makePatchAvatar() {
     return api.patchAvatar(avatar)
-    .then(data => {
-      user.setUserInfo({avatar: data.avatar})
-      user.setUserAvatar();
-    });
+      .then(data => {
+        user.setUserInfo({ avatar: data.avatar })
+        user.setUserAvatar();
+      });
   }
-  handleSubmit(makePatchAvatar, evt, popupEditAvatar);
-};
+  handleFormSubmit(makePatchAvatar, evt, popupEditAvatar); // Исправили вызов функции handleSubmit
+}
 
 function submitDeleteCard(card, evt) {
   // console.log(card);
   function makeDeleteCard() {
     return api.deleteCard(card)
-    .then(() => {
-      console.log(popupConfirm.card);
-      popupConfirm.card.deleteCard()
-    })
+      .then(() => {
+        console.log(popupConfirm.card);
+        popupConfirm.card.deleteCard()
+      })
   }
-  handleSubmit(makeDeleteCard, evt, popupConfirm, 'Удаление...');
-};
+  handleFormSubmit(makeDeleteCard, evt, popupConfirm, 'Удаление...'); // Исправили вызов функции handleSubmit
+}
 
 function handleEditProfile() {
   const data = user.getUserInfo();

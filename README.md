ad13fb02904d831bb6c2a0edf100bd06838eb63c
mikekichikov <123971900+mikekichikov@users.noreply.github.com>
2023-06-17 15:06:43 +0300

    Revert "Feat/mesto update"

--- README.md
+++ README.md
@@ -12,13 +12,5 @@
 - Grid-layouts
 - JavaScript
 - Webpack
-- ООП
-
-### **Проект выполнили:**
-
-- Кичиков Михаил
-  mikekichikov@yandex.ru
-- Ягольников Олег [GitHub](https://github.com/Yagolnikov)
-  yagolnikovoleg@yandex.ru
 
 [Посмотреть проект](https://mikekichikov.github.io/mesto-project/ "Проект на GitHub Pages")
---
+++ src/components/modal.js
@@ -0,0 +1,28 @@
+import { popups } from "./constants";
+
+export function openPopup(element) {
+  element.classList.add('popup_opened');
+  document.addEventListener("keydown", closeByEscape);
+};
+
+export function closePopup(element) {
+  element.classList.remove('popup_opened');
+  document.removeEventListener("keydown", closeByEscape);
+};
+
+export function closeByOverlay() {
+  popups.forEach(item => {
+    item.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('close-button')) {
+          closePopup(item);
+      }
+    })
+  })
+}
+
+export function closeByEscape(evt) {
+  if(evt.key === 'Escape') {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
+  }
+}
